fb3b6f9e3c840b5ac29ad67b7c6f8fba1952673d
Vladyslav Molibozhenko <vlm@sitecore.net>
2017-11-01 11:44:58 +0200

    initial commit

---
+++ src/Sitecore.Support.191359/DataExchange/Providers/Salesforce/Local/Commands/ShowUsageCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.DataExchange.Providers.Salesforce.Local.Commands
+{
+  [Serializable]
+  public class ShowUsageCommand : Sitecore.DataExchange.Providers.Salesforce.Local.Commands.ShowUsageCommand
+  {
+  }
+}
---
+++ src/Sitecore.Support.191359/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.191359")]
+[assembly: AssemblyProduct("Sitecore.Support.191359")]
+[assembly: ComVisible(false)]
